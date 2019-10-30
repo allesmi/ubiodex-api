9d6bc57db5c6c63ba69d9543d25d2b0dc1f30aef
Alexander Miller <alles.mil@gmail.com>
2019-10-30 21:06:20 +0100

    config.py: Add SQLALCHEMY_DATABASE_URI configuration

--- config.py
+++ config.py
@@ -8,6 +8,9 @@ class Config(object):
 	TESTING = False
 	CSRF_ENABLED = True
 	SECRET_KEY = 'Dev'
+	
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite://' + os.path.join(basedir, 'app.db'))
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProductionConfig(Config):
 	CONFIG_NAME = 'Production'
