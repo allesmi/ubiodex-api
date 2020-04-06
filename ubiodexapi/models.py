af761a1c3d8f36583ebb05aaa30c3dac46fbdd13
Alexander Miller <alles.mil@gmail.com>
2020-04-06 16:12:45 +0200

    Simplify database schema

--- ubiodexapi/models.py
+++ ubiodexapi/models.py
@@ -1,42 +1,12 @@
 from . import db, ma
 
-class Rank(object):
-	name = db.Column(db.String(100))
 
-	def __repr__self(self):
-		return f'{self.__name__} "{self.name}"'
+class Taxon(db.Model):
+    # See https://tools.gbif.org/dwca-validator/extension.do?id=dwc:Taxon#Taxon
+    taxonID = db.Column(db.String(100))
+    scientificName = db.Column(db.String(100))
 
-class Kingdom(Rank, db.Model):
-	kingdom_id = db.Column(db.Integer, primary_key = True)
 
-class KingdomSchema(ma.ModelSchema):
-	class Meta:
-		model = Kingdom
-
-class Phylum(Rank, db.Model):
-	phylum_id = db.Column(db.Integer, primary_key = True)
-
-class PhylumSchema(ma.ModelSchema):
-	class Meta:
-		model = Phylum
-
-class Class(Rank, db.Model):
-	class_id = db.Column(db.Integer, primary_key = True)
-
-class ClassSchema(ma.ModelSchema):
-	class Meta:
-		model = Class
-
-class Order(Rank, db.Model):
-	order_id = db.Column(db.Integer, primary_key = True)
-
-class OrderSchema(ma.ModelSchema):
-	class Meta:
-		model = Order
-
-class Family(Rank, db.Model):
-	family_id = db.Column(db.Integer, primary_key = True)
-
-class FamilySchema(ma.ModelSchema):
-	class Meta:
-		model = Family
+class TaxonSchema(ma.ModelSchema):
+    class Meta:
+        model = Taxon
--- ubiodexapi/routes.py
+++ ubiodexapi/routes.py
@@ -2,62 +2,30 @@ from flask import current_app as app
 from flask_restful import Resource, Api
 
 from .models import (
-	Kingdom, KingdomSchema,
-	Phylum, PhylumSchema,
-	Class, ClassSchema,
-	Order, OrderSchema,
-	Family, FamilySchema
+    Taxon, TaxonSchema
 )
 
 API_PREFIX = '/api'
 
 api = Api(app)
 
-class KingdomResource(Resource):
-	def get(self):
-		l = Kingdom.query.all()
 
-		schema = KingdomSchema()
-		return schema.dump(l, many=True)
+class TaxonResource(Resource):
+    def get(self):
+        l = Taxon.query.all()
 
-class PhylumResource(Resource):
-	def get(self):
-		l = Phylum.query.all()
+        schema = TaxonSchema()
+        return schema.dump(l, many=True)
 
-		schema = PhylumSchema()
-		return schema.dump(l, many = True)
-
-class ClassResource(Resource):
-	def get(self):
-		l = Class.query.all()
-
-		schema = ClassSchema()
-		return schema.dump(l, many = True)
-
-class OrderResource(Resource):
-	def get(self):
-		l = Order.query.all()
-
-		schema = OrderSchema()
-		return schema.dump(l, many = True)
-
-class FamilyResource(Resource):
-	def get(self):
-		l = Family.query.all()
-
-		schema = FamilySchema()
-		return schema.dump(l, many = True)
 
 class Test(Resource):
-	def get(self):
-		return { 'hello': 'world' }
+    def get(self):
+        return {'hello': 'world'}
 
-api.add_resource(KingdomResource, f'{API_PREFIX}/kingdom')
-api.add_resource(PhylumResource, f'{API_PREFIX}/phylum')
-api.add_resource(ClassResource, f'{API_PREFIX}/class')
-api.add_resource(OrderResource, f'{API_PREFIX}/order')
-api.add_resource(FamilyResource, f'{API_PREFIX}/family')
+
+api.add_resource(TaxonResource, f'{API_PREFIX}/taxon')
+
 
 @app.route('/')
 def hello():
-	return 'Hello World!'
+    return 'Hello World!'
